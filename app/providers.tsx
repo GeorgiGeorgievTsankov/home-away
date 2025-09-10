ff2f0664623dc457237e9125b9bd487613f07f1b
Georgi Tsankov <multiartdelux@abv.bg>
2025-09-10 17:03:06 +0300

    Fix dropdown menu structure and update providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,16 +1,20 @@
-'use client';
-import { ThemeProvider } from './theme-provider';
+"use client";
+import { ThemeProvider } from "./theme-provider";
+import { Toaster } from "sonner";
 
 function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider
-      attribute='class'
-      defaultTheme='system'
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
+    <>
+      <Toaster richColors position="top-right" />
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </>
   );
 }
 export default Providers;
