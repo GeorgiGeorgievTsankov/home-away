53adce779e3d2631278c6c68e1087c3414022ca3
Georgi Tsankov <multiartdelux@abv.bg>
2025-09-24 17:51:15 +0300

    add formInput

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,30 +1,31 @@
-import FormInput from '@/components/form/FormInput';
-import { SubmitButton } from '@/components/form/Buttons';
-import FormContainer from '@/components/form/FormContainer';
+import FormInput from "@/components/form/FormInput";
+import { SubmitButton } from "@/components/form/Buttons";
+import FormContainer from "@/components/form/FormContainer";
 
 const createProfileAction = async (prevState: any, formData: FormData) => {
-  'use server';
-  const firstName = formData.get('firstName') as string;
-  if (firstName !== 'shakeAndBake') return { message: 'there was an error...' };
-  return { message: 'Profile Created' };
+  "use server";
+  const firstName = formData.get("firstName") as string;
+  if (!firstName || firstName.trim() === "") {
+    return { message: "First name is required." };
+  }
+  return { message: "Profile Created" };
 };
 
 function CreateProfile() {
   return (
     <section>
-      <h1 className='text-2xl font-semibold mb-8 capitalize'>new user</h1>
-      <div className='border p-8 rounded-md max-w-lg'>
+      <h1 className="text-2xl font-semibold mb-8 capitalize">new user</h1>
+      <div className="border p-8 rounded-md max-w-lg">
         <FormContainer action={createProfileAction}>
-          <div className='grid gap-4 mt-4 '>
-            <FormInput type='text' name='firstName' label='First Name' />
-            <FormInput type='text' name='lastName' label='Last Name' />
-            <FormInput type='text' name='username' label='Username' />
+          <div className="grid gap-4 mt-4 ">
+            <FormInput type="text" name="firstName" label="First Name" />
+            <FormInput type="text" name="lastName" label="Last Name" />
+            <FormInput type="text" name="username" label="Username" />
           </div>
-          <SubmitButton text='Create Profile' className='mt-8' />
+          <SubmitButton text="Create Profile" className="mt-8" />
         </FormContainer>
       </div>
     </section>
   );
 }
 export default CreateProfile;
-
--- components/form/FormContainer.tsx
+++ components/form/FormContainer.tsx
@@ -1,29 +1,27 @@
-'use client';
+"use client";
 
-import { useFormState } from 'react-dom';
-import { useEffect } from 'react';
-import { useToast } from '@/components/ui/use-toast';
-import { actionFunction } from '../../utils/types';
+import { useActionState, useEffect } from "react";
+import { useToast } from "@/components/ui/use-toast";
+import { actionFunction } from "../../utils/types";
 
 const initialState = {
-  message: '',
+  message: "",
 };
 
-
 function FormContainer({
-    action,
-    children,
-  }: {
-    action: actionFunction;
-    children: React.ReactNode;
-  }) {
-    const [state, formAction] = useFormState(action, initialState);
-    const { toast } = useToast();
-    useEffect(() => {
-      if (state.message) {
-        toast({ description: state.message });
-      }
-    }, [state]);
-    return <form action={formAction}>{children}</form>;
-  }
-  export default FormContainer;
+  action,
+  children,
+}: {
+  action: actionFunction;
+  children: React.ReactNode;
+}) {
+  const [state, formAction] = useActionState(action, initialState);
+  const { toast } = useToast();
+  useEffect(() => {
+    if (state.message) {
+      toast(state.message);
+    }
+  }, [state]);
+  return <form action={formAction}>{children}</form>;
+}
+export default FormContainer;
