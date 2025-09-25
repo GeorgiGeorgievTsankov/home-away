0250f4aa0cddbda580436fa2a396368f7f309264
Georgi Tsankov <multiartdelux@abv.bg>
2025-09-25 14:48:30 +0300

    add form actions and validation schemas

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,21 +1,16 @@
 import FormInput from "@/components/form/FormInput";
 import { SubmitButton } from "@/components/form/Buttons";
 import FormContainer from "@/components/form/FormContainer";
+import { createProfileAction } from "@/utils/actions";
 
-const createProfileAction = async (prevState: any, formData: FormData) => {
-  "use server";
-  const firstName = formData.get("firstName") as string;
-  if (!firstName || firstName.trim() === "") {
-    return { message: "First name is required." };
-  }
-  return { message: "Profile Created" };
-};
+
+ 
 
 function CreateProfile() {
   return (
     <section>
       <h1 className="text-2xl font-semibold mb-8 capitalize">new user</h1>
-      <div className="border p-8 rounded-md max-w-lg">
+      <div className="border p-8 rounded-md">
         <FormContainer action={createProfileAction}>
           <div className="grid gap-4 mt-4 ">
             <FormInput type="text" name="firstName" label="First Name" />
